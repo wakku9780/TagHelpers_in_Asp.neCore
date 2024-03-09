67834cb3f92b3d895d344433666f650b487773d4
waqar <wakku9780@gmail.com>
2024-03-09 17:01:52 +0530

    completed

---
+++ TagHelpers_in_Asp.neCore/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace TagHelpers_in_Asp.neCore.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Gender gender { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get;set; }
+
+        public string Married { get; set; }
+
+        public string Description { get; set; }
+
+        public enum Gender
+        {
+               Male,Female   
+        }
+    }
+}
